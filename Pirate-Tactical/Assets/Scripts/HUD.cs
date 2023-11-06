8a5449f02c9fedf31a5603fbab796366e82ecfec
LeBud <c.huyghe@rubika-edu.com>
2023-11-06 11:14:37 +0100

    more HUD

--- Pirate-Tactical/Assets/Scripts/HUD.cs
+++ Pirate-Tactical/Assets/Scripts/HUD.cs
@@ -18,11 +18,15 @@ public class HUD : NetworkBehaviour
 
     public TextMeshProUGUI playerName;
     public TextMeshProUGUI enemyPlayerName;
+    public TextMeshProUGUI currentMode;
 
     public GameObject inGameHUD;
 
     Cursor player;
     Cursor enemyPlayer;
+
+    public Transform shipsDisplay;
+    public Transform shipHighlight;
 
     private void Awake()
     {
@@ -35,11 +39,11 @@ public class HUD : NetworkBehaviour
     private void LateUpdate()
     {
         if (player == null) return;
+
         if (player.shipSelected)
         {
             currentShipInfo.text = "ship selected : " + player.unitManager.ships[player.currentShipIndex].unitName + "\nCan move : " + player.unitManager.ships[player.currentShipIndex].canMove.Value 
             + "\nCan shoot : " + player.unitManager.ships[player.currentShipIndex].canShoot.Value
-            + "\nMode Index : " + GetCurrentMode(player.currentModeIndex);
         }
         else
         {
@@ -47,6 +51,28 @@ public class HUD : NetworkBehaviour
         }
 
         specialSlider.value = player.currentSpecialCharge;
+
+        if (player.shipSelected)
+        {
+            shipHighlight.gameObject.SetActive(true);
+            shipHighlight.transform.position = shipsDisplay.GetChild(player.currentShipIndex).transform.position;
+            currentMode.text = GetCurrentMode(player.currentModeIndex);
+        }
+        else
+        {
+            currentMode.text = "none";
+            shipHighlight.gameObject.SetActive(false);
+        }
+
+    }
+
+    void SetShipOnHUD()
+    {
+        for(int i = 0; i<player.unitManager.ships.Length; i++)
+        {
+            shipsDisplay.GetChild(i).GetChild(3).GetComponent<TextMeshProUGUI>().text = player.unitManager.ships[i].unitLife.Value.ToString();
+            shipsDisplay.GetChild(i).GetChild(4).GetComponent<TextMeshProUGUI>().text = player.unitManager.ships[i].damage.ToString();
+        }
     }
 
     string GetCurrentMode(int i)
@@ -80,6 +106,8 @@ public class HUD : NetworkBehaviour
     {
         playerSlider.value = player.totalPlayerHealth.Value;
         enemyPlayerSlider.value = enemyPlayer.totalPlayerHealth.Value;
+
+        SetShipOnHUD();
     }
 
     [ClientRpc]
@@ -116,6 +144,8 @@ public class HUD : NetworkBehaviour
             if (id == 0) enemyPlayerName.text = GameManager.Instance.player1.Value.ToString();
             else if (id == 1) enemyPlayerName.text = GameManager.Instance.player2.Value.ToString();
         }
+
+        SetShipOnHUD();
         
     }
 
